d1fc2fb007a29dd335cf2da7563cee501fab9927
ZaahirZ <zaahirziyaad@gmail.com>
2024-04-02 11:23:39 +0200

    Can write to files and made a new gamefile
    You can write to the data and game file with a list.

--- src/main/java/org/solidereviews/utils/FileManager.java
+++ src/main/java/org/solidereviews/utils/FileManager.java
@@ -1,17 +1,31 @@
 package org.solidereviews.utils;
+
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class FileManager {
-    private static final String DIRECTORY_PATH = FileManager.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-    private static final String FILE_PATH = DIRECTORY_PATH + "data/data.txt";
+
+    private static final String DIRECTORY_PATH = FileManager.class.getProtectionDomain().getCodeSource().getLocation().getPath(); //Location of the code
+    private static final String FILE_PATH1 = DIRECTORY_PATH + "data/data.txt";
+    private static final String FILE_PATH2 = DIRECTORY_PATH + "data/games.txt";//for now the file is located in build
+
+    private final String dataFilePath;
+    private final String gamesFilePath;
+
 
     public FileManager() {
-        MakeDataFile();
+        makeDataFile();
+        makeGamesFile();
+        this.dataFilePath = FILE_PATH1;
+        this.gamesFilePath = FILE_PATH2;
     }
 
-    public void MakeDataFile() {
-        File file = new File(FILE_PATH);
+
+    private void makeDataFile() {
+        File file = new File(FILE_PATH1);
         if (!file.exists()) {
             try {
                 file.getParentFile().mkdirs();
@@ -20,5 +34,37 @@ public class FileManager {
                 e.printStackTrace();
             }
         }
+    } // Makes a data file if it doesn't exist
+
+    private void makeGamesFile() {
+        File file = new File(FILE_PATH2);
+        if (!file.exists()) {
+            try {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    } // Makes a data file if it doesn't exist
+
+    public void writeDataFile(List<String> data) {
+        writeFile(data, dataFilePath);
+    } //file to write to data
+
+    public void writeGamesFile(List<String> games) {
+        writeFile(games, gamesFilePath);
+    } //file to write to games
+
+    private void writeFile(List<String> lines, String filePath) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (String line : lines) {
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
+
