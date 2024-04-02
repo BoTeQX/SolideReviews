004e1269c6830a5ca21fbbeed13019f731f41544
Rody <rody.van.dijk@gmail.com>
2024-04-02 11:51:06 +0200

    Game crud finished (except for input handeling)

--- src/main/java/org/solidereviews/games/GameDisplayer.java
+++ src/main/java/org/solidereviews/games/GameDisplayer.java
@@ -89,9 +89,5 @@ public class GameDisplayer extends GameController{
         }
     }
 
-    private static void clearScreen() {
-        System.out.print("\033\143");
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
+
 }
